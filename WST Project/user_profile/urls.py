3ad43aaa363ea14b81798cdb4582a52c845c4c7c
quincyx1631 <quincyx1631@gmail.com>
2024-11-11 00:10:16 +0800

    hays

--- WST Project/user_profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "profile"
-urlpatterns = [
-    path('', views.view_profile, name='view_profile'),
-    path('update/', views.update_profile, name='update_profile'),
-    path('password/update/', views.update_password, name='update_password'),
-]
